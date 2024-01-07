610f523033c3b2975080173da1f08d7bc94614c4
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2024-01-07 13:51:39 +0900

    Fix test

--- test/catch.el
+++ test/catch.el
@@ -1,18 +1,20 @@
 (require 'yaxception)
 
 (ert-deftest catch/the-error ()
+  "catch the error"
   (let ((i 0))
     (yaxception:$
       (yaxception:try
-        (delete-file "not found")
+        (delete-file 'hoge)
         (setq i 10))
-      (yaxception:catch 'file-error e
-        (incf i 5))
+      (yaxception:catch 'wrong-type-argument e
+        (cl-incf i 5))
       (yaxception:catch 'error e
-        (incf i 2)))
+        (cl-incf i 2)))
     (should (equal i 5))))
 
 (ert-deftest catch/as-ancestor ()
+  "catch as ancestor"
   (unwind-protect
       (progn
         (define-error 'yaxception-test-error "This is YAX test." 'file-error)
@@ -22,8 +24,8 @@
               (yaxception:throw 'yaxception-test-error)
               (setq i 10))
             (yaxception:catch 'file-error e
-              (incf i 5))
+              (cl-incf i 5))
             (yaxception:catch 'error e
-              (incf i 2)))
+              (cl-incf i 2)))
           (should (equal i 5))))
     (setplist 'yaxception-test-error nil)))
